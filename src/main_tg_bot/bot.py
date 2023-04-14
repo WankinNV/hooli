7f9230858cfb7d5b477158ead006620e41772824
WankinNV <Luise_X@mail.ru>
2023-04-14 23:15:18 +0300

    AGAIN

--- src/main_tg_bot/bot.py
+++ src/main_tg_bot/bot.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import BotCommand
 
@@ -12,15 +12,20 @@ storage = MemoryStorage()
 bot = Bot(token=bot_config.token)
 dp = Dispatcher(bot, storage=storage)
 
-
 register_commands_handlers(dp)
 register_like_callbacks(dp)
+
+
+#@dp.message_handler(content_types=["photo"])
+#async def ph(msg: types.Message):
+#    await msg.reply(text='Vot eto krasota')
 
 
 async def set_commands(bot: Bot):
     commands = [
         BotCommand(command="/start", description="go to main menu"),
         BotCommand(command="/help", description="show help"),
+        BotCommand(command="/send", description="send photo to hooliGAN")
     ]
     await bot.set_my_commands(commands)
 
--- src/main_tg_bot/handlers/coammands_handlers.py
+++ src/main_tg_bot/handlers/coammands_handlers.py
@@ -5,6 +5,9 @@ import cv2
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.types import InputMediaPhoto, InputFile
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher.filters import ContentTypeFilter
+from aiogram.types import ContentTypes
 
 from src.main_tg_bot.callbacks.like_callbacks import get_like_kb
 from src.main_tg_bot.menu_texts import help_text
@@ -30,19 +33,34 @@ async def start(message: types.Message, state: FSMContext):
     media_group = types.MediaGroup()
     media_group.attach_photo(InputMediaPhoto(media=InputFile(image_path)))
     await message.answer_photo(photo=InputFile(image_path),
-                               reply_markup= get_like_kb(1))
+                               reply_markup=get_like_kb(1))
 
-    # await message.answer(text='hello')
     os.remove(image_path)
-
-
 
 
 async def help(message: types.Message):
     await message.answer(text=help_text)
 
 
-def register_commands_handlers(dp: Dispatcher):
+# класс для обработки состояния ожидания
 
+class WaitPhoto(StatesGroup):
+    wait_photo = State()
+
+
+# @dp.message_handler(content_types=["photo"])
+#не работает, TODO: дописать
+async def photo_start(message: types.Message, state: FSMContext):
+    await state.reset_state(with_data=True)
+    await message.answer(text='Send photo and wait for magic')
+    #await WaitPhoto.wait_photo.set()
+    await state.set_state(WaitPhoto.wait_photo.state)
+
+    #filter = ContentTypeFilter(['photo'])
+    #message.photo
+    #    await message.answer(text='Красивое')
+
+def register_commands_handlers(dp: Dispatcher):
     dp.register_message_handler(start, commands=["start"], state="*")
     dp.register_message_handler(help, commands=["help"], state="*")
+    dp.register_message_handler(photo_start, commands=["send"])
